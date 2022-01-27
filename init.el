c28736ffd31fc4a843f91044e465b094c4daaa2a
Lewis Collum <lcollum@ford.com>
2022-01-27 10:11:02 -0500

    Added shell (bash) to org languages

--- init.el
+++ init.el
@@ -54,6 +54,7 @@
   (org-babel-do-load-languages
       'org-babel-load-languages
       '((emacs-lisp . t)
-      (python . t)))
+	(python . t)
+	(shell . t)))
 
   (push '("conf-unix" . conf-unix) org-src-lang-modes))
