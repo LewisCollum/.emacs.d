665ecc5868a520492d81c8505d56bb1406af79c7
Lewis Collum <lcollum@ford.com>
2022-02-25 14:10:21 -0500

    Changed "C-tab" to "C-`" for treemacs and removed custom variables

--- init.el
+++ init.el
@@ -123,17 +123,4 @@
 (use-package lsp-treemacs
   :bind
   (:map global-map
-	("C-<tab>" . treemacs)))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(gradle-mode which-key use-package projectile org-bullets ob-async magit lsp-ui lsp-treemacs load-bash-alias ivy exec-path-from-shell doom-themes doom-modeline company-box auto-package-update)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+	("C-`" . treemacs)))
